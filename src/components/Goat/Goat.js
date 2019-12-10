28489a8a5daad2ad5fef13461d639b4c69796d81
Ivan Phelps <ivanniothemannio@gmail.com>
2019-12-09 20:41:38 -0600

    prop-types

--- src/components/Goat/Goat.js
+++ src/components/Goat/Goat.js
@@ -5,8 +5,8 @@ class Goat extends React.Component {
     const { goat } = this.props;
 
     return (
-      <div className="card">
-        <img src={goat.imgUrl} className="card-img-top" alt="card image cap" />
+      <div className="card col-3">
+        <img src={goat.imgUrl} className="card-img-top" alt={goat.description} />
         <div className="card-body">
           <h5 className="card-title">{goat.name}</h5>
           <p className="card-text">{goat.age}</p>
