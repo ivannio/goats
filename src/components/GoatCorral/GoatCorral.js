887b0e6b478a7070a85480c66e9c436a60d76fbc
Ivan Phelps <ivanniothemannio@gmail.com>
2019-12-09 20:19:20 -0600

    redering goats

--- src/components/GoatCorral/GoatCorral.js
+++ src/components/GoatCorral/GoatCorral.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Goat from '../Goat/Goat';
 
 class GoatCorral extends React.Component {
   render() {
     const myGoats = this.props.butts;
+
+    const goatCards = myGoats.map((goat) => <Goat key={goat.id} goat={goat}/>);
     return (
       <div className="goatCorral">
-        Goat Butts
+        {goatCards}
     </div>
     );
   }
