3317cabbc7f1590d19c880b66d2226342e743b6b
Ivan Phelps <ivanniothemannio@gmail.com>
2019-12-09 19:47:10 -0600

    state and props

---
+++ src/components/GoatCorral/GoatCorral.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class GoatCorral extends React.Component {
+  render() {
+    const myGoats = this.props.butts;
+    return (
+      <div className="goatCorral">
+        Goat Butts
+    </div>
+    );
+  }
+}
+
+export default GoatCorral;
